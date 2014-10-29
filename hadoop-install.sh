2b710bbb6c603aded0af8f834a450832b50e942b
Talha Ansari <talhajansari@hotmail.com>
2014-10-29 17:11:21 -0400

    Update hadoop-install.sh

--- hadoop-install.sh
+++ hadoop-install.sh
@@ -8,9 +8,9 @@ sudo apt-get upgrade
 sudo apt-get update
 sudo apt-get install build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
 git clone https://github.com/Homebrew/linuxbrew.git ~/.linuxbrew
-export PATH=\"$HOME/.linuxbrew/bin:$PATH\"
-export MANPATH=\"$HOME/.linuxbrew/share/man:$MANPATH\"
-export INFOPATH=\"$HOME/.linuxbrew/share/info:$INFOPATH\"
+export PATH="$HOME/.linuxbrew/bin:$PATH"
+export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
+export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
 echo "export PATH=\"$HOME/.linuxbrew/bin:$PATH\"" >> ~/.bashrc
 echo "export MANPATH=\"$HOME/.linuxbrew/share/man:$MANPATH\"" >> ~/.bashrc
 echo "export INFOPATH=\"$HOME/.linuxbrew/share/info:$INFOPATH\"" >> ~/.bashrc
