00287a8ebecb01d3fd4b0c13660dd076c0135591
talhajansari <talhajansari@hotmail.com>
2014-11-11 21:45:20 -0500

    Added lines to set JAVA_HOME in yarn-env.sh - not tested yet.

--- hadoop-install.sh
+++ hadoop-install.sh
@@ -4,7 +4,7 @@ echo "This script installs Hadoop on Ubuntu."
 echo "The script is starting..."
 
 echo "Installing required dependencies.."
-sudo apt-get upgrade
+#sudo apt-get upgrade
 sudo apt-get update
 sudo apt-get install build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
 git clone https://github.com/Homebrew/linuxbrew.git ~/.linuxbrew
@@ -27,7 +27,12 @@ source ~/.bashrc
 echo "Installing JAVA...(even if you already have it)"
 sudo apt-get install default-jre
 mv /usr/local/Cellar/hadoop/2.5.1/libexec/etc/hadoop/hadoop-env.sh /usr/local/Cellar/hadoop/2.5.1/libexec/etc/hadoop/hadoop-env.sh.backup
+echo "Updating JAVA_HOME in hadoop-env.sh"
 sed '25c\'"export JAVA_HOME=/usr/lib/jvm/default-java/jre/" /usr/local/Cellar/hadoop/2.5.1/libexec/etc/hadoop/hadoop-env.sh.backup >> /usr/local/Cellar/hadoop/2.5.1/libexec/etc/hadoop/hadoop-env.sh
+echo "Updating JAVA_HOME in yarn-env.sh"
+mv /usr/local/Cellar/hadoop/2.5.1/libexec/etc/hadoop/yarn-env.sh /usr/local/Cellar/hadoop/2.5.1/libexec/etc/hadoop/yarn-env.sh.backup
+sed '23c\'"export JAVA_HOME=/usr/lib/jvm/default-java/jre/" /usr/local/Cellar/hadoop/2.5.1/libexec/etc/hadoop/yarn-env.sh.backup >> /usr/local/Cellar/hadoop/2.5.1/libexec/etc/hadoop/yarn-env.sh
+
 echo "."
 echo "."
 echo "."
